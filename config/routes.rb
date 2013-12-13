a094b9aa5624a9220a7ffd7dec8631d20dc3537d
Tim Macdonald <tim_macdonald@hotmail.com>
2013-12-13 17:10:49 +1300

    lololpush

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Omrails::Application.routes.draw do
   resources :articles
     root to: "articles#index"
-    end
+  
   end
-end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
