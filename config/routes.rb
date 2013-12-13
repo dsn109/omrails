4bce17d3c32d46851c8f92b0fd2d5666387a3b41
Tim Macdonald <tim_macdonald@hotmail.com>
2013-12-13 17:01:51 +1300

    Changing routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Omrails::Application.routes.draw do
-  resources :leads, path: ''
+  resources :articles
+  root to: "articles#index"
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
