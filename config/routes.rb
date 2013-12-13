04adf531eb85dc4edb957fb895376640fc6850bd
Tim Macdonald <tim_macdonald@hotmail.com>
2013-12-13 17:15:28 +1300

    not sure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Omrails::Application.routes.draw do
-  resources :leads, path: ''
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
